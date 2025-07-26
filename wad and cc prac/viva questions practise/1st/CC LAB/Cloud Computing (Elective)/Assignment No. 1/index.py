673ea7742842b6f65ad8057542276a02c5dbc59f
Akshit Kotnala <151994823+Akshit7kotnala@users.noreply.github.com>
2025-07-26 11:12:07 +0530

    Delete wad and cc prac/viva questions practise directory

--- wad and cc prac/viva questions practise/1st/CC LAB/Cloud Computing (Elective)/Assignment No. 1/index.py
+++
@@ -1,6 +0,0 @@
-print 'Content-Type: text/plain'
-print ''
-print 'Hello there Friends'
-
-for x in range(1,11):
-	print '5 X ',x,' = ',5*x
--- wad and cc prac/viva questions practise/2nd/Cloud-Computing-main/Cloud Computing/table-app/table-app/main.py
+++
@@ -1,9 +0,0 @@
-print 'Content-Type: text/plain'
-print ''
-print 'Hello World\n'
-
-# Multiplication Table of 5
-for x in range(1, 11):
-    print '5 X', x, '= ', 5 * x
-
-print ''
